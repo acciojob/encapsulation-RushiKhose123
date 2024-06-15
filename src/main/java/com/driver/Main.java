055cda3f93df75b51cb34491a9235b1b495c2cab
RushiKhose123 <rushikeshkhose123@gmail.com>
2024-06-15 14:37:24 +0530

    Encapsulation assignment done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly rwonly = new RWOnly();
+
+        // rwonly.name = "Rushikesh";
+        // System.out.println(rwonly.name);
+
+        rwonly.setName("Rushikesh");
+        System.out.println(rwonly.getName());
+    }
 }
